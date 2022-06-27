59f6f78d4eb0717b7eb3459caee9cb2a9b1b9ede
LuisDet <luisedenist@gmail.com>
2022-06-27 00:07:12 -0400

    mejora

--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,9 @@ var flkty = new Flickity('.carousel', {
   pageDots: false,
   wrapAround: true
 })
+
+document.querySelectorAll('.nav-bar-item').forEach(el =>
+  el.addEventListener('click', () => {
+    document.getElementById('menu').checked = false
+  })
+)
